8d6d8db54677dc3e3bed3b7ca7889991c1ebdf5e
HoshinoHitomi <hiikochan711@gmail.com>
2021-05-29 19:14:31 +0000

    [Update]アプリケーションをつくろう2　3章学習中

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ed451f6783f42563b5f47f473e233db9ce512579790f5f86a37142d12fa0bd0b38685c9d5b6049fde2972ef171c79a1eabfa1b55985ad2502e746e05ad98f278'
